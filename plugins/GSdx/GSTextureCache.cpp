6916211c61bb4d375b9a0c0e4b2c176dc48b836b
Gregory Hainaut <gregory.hainaut@gmail.com>
2015-09-08 13:06:31 +0200

    gsdx: support RGB5A1 <=> depth conversion in texture cache
    Fix bad depth effect in Silent Hill 4

--- plugins/GSdx/GSTextureCache.cpp
+++ plugins/GSdx/GSTextureCache.cpp
@@ -289,14 +289,16 @@ GSTextureCache::Target* GSTextureCache::LookupTarget(const GIFRegTEX0& TEX0, int
 				dst = CreateTarget(TEX0, w, h, type);
 				dst->m_32_bits_fmt = t->m_32_bits_fmt;
 
+				int shader;
+				bool fmt_16_bits = (GSLocalMemory::m_psm[TEX0.PSM].fmt == 2 && GSLocalMemory::m_psm[t->m_TEX0.PSM].fmt == 2);
 				if (type == DepthStencil) {
-					GL_CACHE("TC: Lookup Target(Depth) %dx%d, hit Color (0x%x, F:0x%x)", w, h, bp, TEX0.PSM);
-					int shader = ShaderConvert_RGBA8_TO_FLOAT32 + GSLocalMemory::m_psm[TEX0.PSM].fmt;
-					m_renderer->m_dev->StretchRect(t->m_texture, sRect, dst->m_texture, dRect, shader, false);
+					GL_CACHE("TC: Lookup Target(Depth) %dx%d, hit Color (0x%x, F:0x%x, old F:0x%x)", w, h, bp, TEX0.PSM, t->m_TEX0.PSM);
+					shader = (fmt_16_bits) ? ShaderConvert_RGB5A1_TO_FLOAT16 : ShaderConvert_RGBA8_TO_FLOAT32 + GSLocalMemory::m_psm[TEX0.PSM].fmt;
 				} else {
-					GL_CACHE("TC: Lookup Target(Color) %dx%d, hit Depth (0x%x, F:0x%x)", w, h, bp, TEX0.PSM);
-					m_renderer->m_dev->StretchRect(t->m_texture, sRect, dst->m_texture, dRect, ShaderConvert_FLOAT32_TO_RGBA8, false);
+					GL_CACHE("TC: Lookup Target(Color) %dx%d, hit Depth (0x%x, F:0x%x, old F:0x%x)", w, h, bp, TEX0.PSM, t->m_TEX0.PSM);
+					shader = (fmt_16_bits) ? ShaderConvert_FLOAT16_TO_RGB5A1 : ShaderConvert_FLOAT32_TO_RGBA8;
 				}
+				m_renderer->m_dev->StretchRect(t->m_texture, sRect, dst->m_texture, dRect, shader, false);
 
 				break;
 			}
@@ -862,7 +864,10 @@ GSTextureCache::Source* GSTextureCache::CreateSource(const GIFRegTEX0& TEX0, con
 	{
 		// TODO: clean up this mess
 
-		int shader = dst->m_type != RenderTarget ? ShaderConvert_FLOAT32_TO_RGBA8 : ShaderConvert_COPY;
+		int shader = ShaderConvert_COPY;
+		if (dst->m_type != RenderTarget) {
+			shader = (psm.fmt == 2) ? ShaderConvert_FLOAT16_TO_RGB5A1 : ShaderConvert_FLOAT32_TO_RGBA8;
+		}
 		bool is_8bits = TEX0.PSM == PSM_PSMT8 && IsOpenGL();
 
 		if (is_8bits) {
